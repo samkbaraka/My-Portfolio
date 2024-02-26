abe80d68913a8e315662912c296d73ef1b66f02a
samkbaraka <samkbaraka@gmail.com>
2024-02-26 05:14:55 -0600

    new changes

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add click event listeners to each navbar item
+document.getElementById('home').addEventListener('click', function() {
+    window.location = '#HOME';
+  });
+
+  document.getElementById('about').addEventListener('click', function() {
+    window.location = '#ABOUT';
+  });
+
+  document.getElementById('resume').addEventListener('click', function() {
+    window.location = '#RESUME';
+  });
+
+  document.getElementById('contact').addEventListener('click', function() {
+    window.location = '#contact';
+  });
